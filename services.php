f9becc6735b169660cfaf67b4a8108bdf5869f5d
Blake Hooks <bhooks1234@gmail.com>
2013-01-20 11:09:52 -0500

    Content Copy
    Added contact for Services page. Also added blank page for Portfolio

---
+++ services.php
@@ -0,0 +1,85 @@
+<?php
+$extra_head = '<link rel="stylesheet" type="text/css" href="includes/style.css" />';
+include('includes/head.inc');
+?>
+<h1>Our Services</h1>
+				<h2>
+	Repair Services</h2>
+<p>
+	&nbsp;</p>
+<h4>
+	Misc. Fees:</h4>
+<p>
+	20KM Distance: $20<br />
+	50KM Distance: $50&nbsp;<br />
+	Diagnostic Fee: $20 (waived with Complete Repair Package)<br />
+	Computer Recycling: <strong>FREE</strong></p>
+<h4>
+	Computer Services</h4>
+<p>
+	Software Install: $5<br />
+	Dusting/Physical Cleaning (minor): $15<br />
+	Minor Component Install: $15<br />
+	Major Component Install: $25<br />
+	Tuneup: $25<br />
+	Major Laptop Physical Cleaning: $30<br />
+	Virus Removal: $40<br />
+	System Wipe: $50<br />
+	Cusomization/Mod: $50</p>
+<h4>
+	Complete Repair Package: $75</h4>
+<pre>
+<p>
+	(System Wipe + Physical Cleaning + Data Backup + Peripheral Install + FREE Diag)</p></pre>
+<p>
+	&nbsp;</p>
+<h4>
+	Console Repairs</h4>
+<p>
+	Xbox 360 2 Red Lights Repair: $20</p>
+<p>
+	&nbsp;</p>
+<h4>
+	Custom Build Services</h4>
+<p>
+	Consultation/Estimate: $20 (waived with Custom Build Package) &nbsp;<br />
+	Data Migration: $20&nbsp;<br />
+	Driver Install: $25<br />
+	Operating System Install: $50&nbsp;<br />
+	System Assembly: $50</p>
+<h4>
+	Custom Build Package: $100</h4>
+<pre>
+<p>
+	(Consultation + System Assembly + O/S Install + Software + Data Migration)</p></pre>
+<p>
+	&nbsp;</p>
+<h4>
+	Custom Mac (Hackintosh) Builds</h4>
+<p>
+	Consultation/Estimate: $20 (waived with CustoHack Complete Package)&nbsp;<br />
+	Data Migration: $20<br />
+	Software Installation/Updates: $20<br />
+	Kext Install: $25<br />
+	OS X 10.8.x Install: $50&nbsp;<br />
+	System Assembly: $50</p>
+<h4>
+	CustoHack Complete (Hackintosh): $125</h4>
+<pre>
+<p>
+	(Consultation + System Assembly + O/S Install + Software + Kexts + Data Migration)</p></pre>
+<p>
+	&nbsp;</p>
+<h4>
+	Technology Consulting</h4>
+<p>
+	Always up-to-date with the cutting edge of technology. Let us help you equip your budding business or overhaul and customize your office!</p>
+<p>
+	Please contact for more details.</p>
+<h4>
+	No-Fix/No-Pay &amp; Labour-Protect</h4>
+<p>
+	We pride ourselves on our service. Plain and simple. If we can&#39;t resolve the issue, you don&#39;t pay*</p>
+<p>
+	We also strive to remain competitive. If you find a less expensive advertised price; we&#39;ll beat it! Just bring us the ad.</p>
+<?php include('includes/foot.inc');?>
