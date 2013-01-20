36d02c91033328c379ecb58ff49a34589794bc9d
Blake Hooks <bhooks1234@gmail.com>
2013-01-20 11:13:37 -0500

    Nav Addition
    Added nav include and nav variable to new pages (About, Contact,
    Portfolio, Seminars, and Services)

--- about.php
+++ about.php
@@ -1,6 +1,8 @@
 <?php
 $extra_head = '<link rel="stylesheet" type="text/css" href="includes/style.css" />';
 include('includes/head.inc');
+$nav = 2;
+include('includes/nav.inc'); 
 ?>
 <h1>About Us</h1>
 				<h2>
--- services.php
+++ services.php
@@ -1,6 +1,8 @@
 <?php
 $extra_head = '<link rel="stylesheet" type="text/css" href="includes/style.css" />';
 include('includes/head.inc');
+$nav = 1;
+include('includes/nav.inc'); 
 ?>
 <h1>Our Services</h1>
 				<h2>
