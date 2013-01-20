bdeeab78303f3eddfd4e30240d067cfc1600fce5
Kenny Jones <knvjones@gmail.com>
2013-01-20 00:42:49 -0500

    Added mission
    will add animations on hover later

--- index.php
+++ index.php
@@ -10,5 +10,8 @@ include('includes/nav.inc');
 <img src="images/mission.png" alt="Our Mission" style="width:1100px; margin-left:2px; margin-right:2px;" />
 <div id="slide_frame"></div>
 </div>
+<div id="mission">
+From <span class="blue">educating</span> and <span class="blue">consulting</span>, <span class="orange">customization</span> and <span class="orange">creation</span>, to <span class="green">diagnosis</span> and <span class="green">repair</span>; our team of experts does it <span style="font-family:'RobotoCondensed', sans-serif;">all!</span>
+</div>
 
 <?php include('includes/foot.inc');
