b5f3242118e353d30ab120c6bb4c9cce03407e81
Blake Hooks <bhooks1234@gmail.com>
2013-01-20 11:08:42 -0500

    Content Copy
    Added content to Seminars page

---
+++ seminars.php
@@ -0,0 +1,28 @@
+<?php
+$extra_head = '<link rel="stylesheet" type="text/css" href="includes/style.css" />';
+include('includes/head.inc');
+?>
+<h1>Seminars</h1>
+				<h2>
+	The Challenge of Technology:</h2>
+<br />
+<br />
+<p>
+	We know how turbulent the technology market can be.&nbsp; Finding a balance between work, home, and everything else in between is consistently becoming harder and harder.&nbsp; With ever-changing and fast-paced life-styles it really becomes a challenge to remain on top of the latest trends in the global markets.&nbsp; This is where LiquidTec comes in. Our niche is our ability to consistently monitor patters and remain up-to-date with the latest &quot;gizmos&quot; arising daily in the field of technology! &nbsp;<br />
+	<br />
+	We are confident in our knowledge and ability to translate complex technical jargon into layman language ready for consumption by people of all age groups and technical background.<br />
+	<br />
+	For more information on training seminar services please contact;</p>
+<p>
+	&nbsp;</p>
+<p>
+	&nbsp;</p>
+<pre>
+</pre>
+<p>
+	Sasa Rajic President LiquidTec 519-897-9277 liquidtec@outlook.com</p>
+<pre>
+</pre>
+<p>
+	&nbsp;</p>
+<?php include('includes/foot.inc');?>
