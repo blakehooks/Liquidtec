8ce713e63deacde146f5d4d0ee10fbc3afc4d203
Kenny Jones <knvjones@gmail.com>
2013-01-20 00:56:48 -0500

    Added box options

--- index.php
+++ index.php
@@ -13,5 +13,8 @@ include('includes/nav.inc');
 <div id="mission">
 From <span class="blue">educating</span> and <span class="blue">consulting</span>, <span class="orange">customization</span> and <span class="orange">creation</span>, to <span class="green">diagnosis</span> and <span class="green">repair</span>; our team of experts does it <span style="font-family:'RobotoCondensed', sans-serif;">all!</span>
 </div>
+<div id="option_slider">
+
+</div>
 
 <?php include('includes/foot.inc');
