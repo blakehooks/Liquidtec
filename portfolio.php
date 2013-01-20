6f3e4d2be055c0d554e55a5eeb0e3058d3435752
Blake Hooks <bhooks1234@gmail.com>
2013-01-20 11:11:08 -0500

    Content Copy
    Added content for Portfolio page

---
+++ portfolio.php
@@ -0,0 +1,107 @@
+<?php
+$extra_head = '<link rel="stylesheet" type="text/css" href="includes/style.css" />';
+include('includes/head.inc');
+?>
+<h1>Portfolio</h1>
+				<h3>
+	CustoHack Build 1</h3>
+<p>
+	<img alt="Custom Mac Build 1" href="http://liquidtec.ca/data/uploads/screen-shot-2013-01-08-at-11.10.38-am.png" src="http://liquidtec.ca/data/uploads/screen-shot-2013-01-08-at-11.10.38-am.png" style="width: 600px; height: 338px; " /></p>
+<pre>
+<p>
+	Intel Core i7 2600K<br />
+	ASUS P8Z68-V PRO Motherboard<br />
+	16/32 GB Corsair Vengeance<br />
+	ASUS GeForce GT 560 1 GB GDDR5 Video Card<br />
+	4x HDD (1 TB Each)<br />
+	Tri-Booting OS X 10.8.2 &amp; Windows 8 Professional /w Media Center &amp; Windows 7 Ultimate</p>
+</pre>
+<h3>
+	Custom Gaming Rig Mid 2012</h3>
+<p>
+	<img alt="Boris" src="http://liquidtec.ca/data/uploads/doobzpc.png" style="width: 600px; height: 338px;" /><br />
+	<br />
+	<img alt="Boris' Rig" src="http://liquidtec.ca/data/uploads/img_0910.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0912.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0915.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0914.jpg" style="width: 600px; height: 400px;" /></p>
+<pre>
+<p>
+	Intel Core i5 3570K&nbsp;<br />
+	Gigabyte GA-Z77 Motherboard<br />
+	8 GB Corsair Vengeance<br />
+	EVGA GeForce GTX 570<br />
+	1 TB HDD<br />
+	Windows 8 Professional /w Media Center&nbsp;</p>
+</pre>
+<br />
+<h3>
+	CustoHack Early 2013</h3>
+<p>
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0916.jpg" style="width: 600px; height: 400px; " /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0917.jpg" style="width: 600px; height: 400px; " /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0918.jpg" style="width: 600px; height: 400px; " /><br />
+	<br />
+	&nbsp;</p>
+<pre>
+<p>
+	Intel Core i7 2600K &nbsp;<br />
+	ASUS Republic Of Gamers<br />
+	RAMPAGE Motherboard<br />
+	Gigabyte GTX 480 /w Zallman Turbo-Cooler<br />
+	Corsair H70 CPU Liquid Cooling Block<br />
+	24 GB Corsair Vengeance RAM<br />
+	5 Point Thermal/Fan Digital Controller<br />
+	1200W Corsair PSU<br />
+	RAID Hard Drive Array<br />
+	OS X 10.8.2</p>
+</pre>
+<p>
+	&nbsp;</p>
+<h3>
+	Budget Build #1 - Early 2013</h3>
+<p>
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0930.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0921.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/img_0926.jpg" style="width: 600px; height: 400px;" /></p>
+<pre>
+<p>
+	AMD A6-3670K&nbsp;<br />
+	GA-AA55 Motherboard<br />
+	Saphire AMD 6550 Video Card<br />
+	8 GB Mushkin RAM<br />
+	500 GB HDD<br />
+	Windows 7 Professional</p>
+</pre>
+<p>
+	&nbsp;</p>
+<h3>
+	Custom Gaming Rig Mid 2012 # 2</h3>
+<p>
+	<img alt="" src="http://liquidtec.ca/data/uploads/bzfront.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/bzi5.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/bzproc.jpg" style="width: 600px; height: 400px;" /><br />
+	<br />
+	<img alt="" src="http://liquidtec.ca/data/uploads/bzinside.jpg" style="width: 600px; height: 400px;" /></p>
+<pre>
+<p>
+	Intel Core i5 - 2500K<br />
+	ASUS P8z77 Motherboard<br />
+	EVGA GeForce 570 CLASSIFIED Edition<br />
+	8 GB RAM<br />
+	OCZ 600 Watt Modular PSU<br />
+	120 GB SSD<br />
+	1 TB HDD<br />
+	WIndows 7 Ultimate&nbsp;</p>
+</pre>
+<?php include('includes/foot.inc');?>
