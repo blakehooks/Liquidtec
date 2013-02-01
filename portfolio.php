49d68f6c5b65355f30d745132347c58eb55530bd
Kenny Jones <knvjones@gmail.com>
2013-02-01 16:19:30 -0500

    Added return scroll, should be last.

--- portfolio.php
+++ portfolio.php
@@ -105,6 +105,7 @@ $(document).ready(function(){
 		id = parseInt(id);
 		if (id >= photo_gallery.set_size) {
 			id = 0;
+			photo_gallery.scroll_miniset(-200); // changes the minibar to move forward with the picture.
 		}
 		id++;
 		if (id > 2) {
